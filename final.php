63b5ea54c46072a100f84fb5949adc933ce28959
mindziukasss <mindziukasss@gmail.com>
2017-02-24 11:03:17 +0200

    Added get up files

---
+++ final.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Quizzer</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+  <header>
+    <div class="container">
+      <h1>PHP Quizzer</h1>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+      <h2>You're Done!</h2>
+      <p>Congrats! You have completed the test</p>
+      <p>Final Score: 5</p>
+      <a href="question.php?n=1" class="start">Take Again</a>
+    </div>
+  </main>
+  <footer>
+    <div class="container">
+      <h3>Copyrigth &copy;2017, PHP Quizzer</h3>
+    </div>
+  </footer>
+</body>
+</html>
